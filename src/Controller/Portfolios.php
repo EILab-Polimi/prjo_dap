c8e0b39d4a756c200b61a859096d649aa1b7995c
ilpise <pisewip@yahoo.it>
2022-04-19 12:07:26 +0200

    Db

--- src/Controller/Portfolios.php
+++ src/Controller/Portfolios.php
@@ -24,7 +24,7 @@ class Portfolios extends ControllerBase {
 
     // Use external database
     // db_set_active('prjo_dap_v2');
-    $connection = Database::getConnection('prjo_dap', 'default');
+    $connection = Database::getConnection('default','prjo_dap');
     $query_test = $connection->query("SELECT DISTINCT storage.exp AS storage_exp
                             FROM
                             {storage} storage");
