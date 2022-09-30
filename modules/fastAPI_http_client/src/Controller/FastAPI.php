4e2fd1024ada45ef4c114129b2bad058f3ac39d3
ilpise <pisewip@yahoo.it>
2022-09-30 17:52:05 +0200

    Added routes with slug and GET parametrs management

--- modules/fastAPI_http_client/src/Controller/FastAPI.php
+++ modules/fastAPI_http_client/src/Controller/FastAPI.php
@@ -11,6 +11,8 @@ use Drupal\Component\Serialization\Json as Json;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
 
 
 /**
@@ -50,25 +52,81 @@ class FastAPI extends ControllerBase {
   public function getData() {
 
 
-    // USE IN PRODUCTION (DOCKER STACK)
-    // $request = $this->httpClient->request('GET', 'http://DAP_fastapi:8000');
+    $build = [
+      '#theme' => 'afclm',
+    ];
+
+
+    \Drupal::service('messenger')->addMessage("<code>".print_r($remoteurl,TRUE)."</code>");
+
+    return $build;
+
+    // // USE IN PRODUCTION (DOCKER STACK)
+    // // $request = $this->httpClient->request('GET', 'http://DAP_fastapi:8000');
+    // $request = $this->httpClient->request('GET', 'http://localhost:5000/portfolios');
+    //
+    // // \Drupal::service('messenger')->addMessage("<code>".print_r($request->getStatusCode(),TRUE)."</code>");
+    //
+    // $status = $request->getStatusCode();
+    //
+    // $body = $request->getBody();
+    // // \Drupal::service('messenger')->addMessage("<code>".print_r($body,TRUE)."</code>");
+    //
+    // $contents = $body->getContents();
+    // // \Drupal::service('messenger')->addMessage("<code>".print_r($contents,TRUE)."</code>");
+    //
+    // // transform $contents to array !!!
+    // // Dipende perchè non tutte le response di fastAPI sono json
+    //
+    // // 1- $contents viene jsonencodato 2 volte
+    //
+    // return new JsonResponse([ 'data' => $contents, 'method' => 'GET', 'status'=> $status]);
+
+  }
+
+  public function portfolios() {
     $request = $this->httpClient->request('GET', 'http://localhost:5000/portfolios');
-
-    // \Drupal::service('messenger')->addMessage("<code>".print_r($request->getStatusCode(),TRUE)."</code>");
-
     $status = $request->getStatusCode();
-
     $body = $request->getBody();
-    // \Drupal::service('messenger')->addMessage("<code>".print_r($body,TRUE)."</code>");
-
     $contents = $body->getContents();
-    // \Drupal::service('messenger')->addMessage("<code>".print_r($contents,TRUE)."</code>");
 
     // transform $contents to array !!!
     // Dipende perchè non tutte le response di fastAPI sono json
 
-    return new JsonResponse([ 'data' => $contents, 'method' => 'GET', 'status'=> $status]);
+    // 1- $contents viene jsonencodato 2 volte
 
+    return new JsonResponse([ 'data' => $contents, 'method' => 'GET', 'status'=> $status]);
+  }
+
+  public function indicators($ind_route, Request $request) {
+
+    $build = [
+      '#theme' => 'afclm',
+    ];
+
+    \Drupal::service('messenger')->addMessage("<code>".print_r($ind_route,TRUE)."</code>");
+
+    // \Drupal::service('messenger')->addMessage("<code>".print_r($request,TRUE)."</code>");
+
+    // https://drupal.stackexchange.com/questions/207044/how-to-get-post-and-get-parameters
+    // $test = $request->query->get('cesare');
+    // GET all the GET parametrs from incoming request
+    $test = $request->query->all();
+
+    \Drupal::service('messenger')->addMessage("<code>".print_r($test,TRUE)."</code>");
+    return $build;
+
+    // $request = $this->httpClient->request('GET', 'http://localhost:5000/portfolios');
+    // $status = $request->getStatusCode();
+    // $body = $request->getBody();
+    // $contents = $body->getContents();
+    //
+    // // transform $contents to array !!!
+    // // Dipende perchè non tutte le response di fastAPI sono json
+    //
+    // // 1- $contents viene jsonencodato 2 volte
+    //
+    // return new JsonResponse([ 'data' => ["test" => "test"], 'method' => 'GET', 'status'=> 200]);
   }
 
 }
