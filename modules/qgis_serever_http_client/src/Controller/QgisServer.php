42af0312c1e47aac58c9a3709f2e0859b178fc44
ilpise <pisewip@yahoo.it>
2022-11-04 14:42:59 +0100

    Removed test url for QgisServer proxy

--- modules/qgis_serever_http_client/src/Controller/QgisServer.php
+++ modules/qgis_serever_http_client/src/Controller/QgisServer.php
@@ -45,62 +45,62 @@ class QgisServer extends ControllerBase {
     }
 
 
-    public function test() {
-
-
-      $build = [
-        '#theme' => 'afclm',
-      ];
-
-
-      // \Drupal::service('messenger')->addMessage("<code>".print_r($remoteurl,TRUE)."</code>");
-
-      $config = \Drupal::config('geoviz.settings');
-      if ($config->get('qgis_server_sel') == 0){
-        $QgisUrl = $config->get('qgis_server_dev_url');
-      } else {
-        $QgisUrl = $config->get('qgis_server_prod_url');
-      }
-
-      // \Drupal::service('messenger')->addMessage("<code>".print_r($ind_route,TRUE)."</code>");
-
-      \Drupal::service('messenger')->addMessage("<code>".print_r($request,TRUE)."</code>");
-
-      // https://drupal.stackexchange.com/questions/207044/how-to-get-post-and-get-parameters
-      // $GET_params = $request->query->get('cesare');
-      // GET all the GET parametrs from incoming request
-      $GET_params = $request->query->all();
-      // \Drupal::service('messenger')->addMessage("<code>".print_r($GET_params,TRUE)."</code>");
-
-      $url =  $fastAPIServerBaseUrl.'/indicators/'.$ind_route.'?'.http_build_query($GET_params);
-
-      \Drupal::service('messenger')->addMessage("<code>".print_r($url,TRUE)."</code>");
-
-
-      return $build;
-
-      // // USE IN PRODUCTION (DOCKER STACK)
-      // // $request = $this->httpClient->request('GET', 'http://DAP_fastapi:8000');
-      // $request = $this->httpClient->request('GET', 'http://localhost:5000/portfolios');
-      //
-      // // \Drupal::service('messenger')->addMessage("<code>".print_r($request->getStatusCode(),TRUE)."</code>");
-      //
-      // $status = $request->getStatusCode();
-      //
-      // $body = $request->getBody();
-      // // \Drupal::service('messenger')->addMessage("<code>".print_r($body,TRUE)."</code>");
-      //
-      // $contents = $body->getContents();
-      // // \Drupal::service('messenger')->addMessage("<code>".print_r($contents,TRUE)."</code>");
-      //
-      // // transform $contents to array !!!
-      // // Dipende perchè non tutte le response di fastAPI sono json
-      //
-      // // 1- $contents viene jsonencodato 2 volte
-      //
-      // return new JsonResponse([ 'data' => $contents, 'method' => 'GET', 'status'=> $status]);
-
-    }
+    // public function test() {
+    //
+    //
+    //   $build = [
+    //     '#theme' => 'afclm',
+    //   ];
+    //
+    //
+    //   // \Drupal::service('messenger')->addMessage("<code>".print_r($remoteurl,TRUE)."</code>");
+    //
+    //   $config = \Drupal::config('geoviz.settings');
+    //   if ($config->get('qgis_server_sel') == 0){
+    //     $QgisUrl = $config->get('qgis_server_dev_url');
+    //   } else {
+    //     $QgisUrl = $config->get('qgis_server_prod_url');
+    //   }
+    //
+    //   // \Drupal::service('messenger')->addMessage("<code>".print_r($ind_route,TRUE)."</code>");
+    //
+    //   \Drupal::service('messenger')->addMessage("<code>".print_r($request,TRUE)."</code>");
+    //
+    //   // https://drupal.stackexchange.com/questions/207044/how-to-get-post-and-get-parameters
+    //   // $GET_params = $request->query->get('cesare');
+    //   // GET all the GET parametrs from incoming request
+    //   $GET_params = $request->query->all();
+    //   // \Drupal::service('messenger')->addMessage("<code>".print_r($GET_params,TRUE)."</code>");
+    //
+    //   $url =  $fastAPIServerBaseUrl.'/indicators/'.$ind_route.'?'.http_build_query($GET_params);
+    //
+    //   \Drupal::service('messenger')->addMessage("<code>".print_r($url,TRUE)."</code>");
+    //
+    //
+    //   return $build;
+    //
+    //   // // USE IN PRODUCTION (DOCKER STACK)
+    //   // // $request = $this->httpClient->request('GET', 'http://DAP_fastapi:8000');
+    //   // $request = $this->httpClient->request('GET', 'http://localhost:5000/portfolios');
+    //   //
+    //   // // \Drupal::service('messenger')->addMessage("<code>".print_r($request->getStatusCode(),TRUE)."</code>");
+    //   //
+    //   // $status = $request->getStatusCode();
+    //   //
+    //   // $body = $request->getBody();
+    //   // // \Drupal::service('messenger')->addMessage("<code>".print_r($body,TRUE)."</code>");
+    //   //
+    //   // $contents = $body->getContents();
+    //   // // \Drupal::service('messenger')->addMessage("<code>".print_r($contents,TRUE)."</code>");
+    //   //
+    //   // // transform $contents to array !!!
+    //   // // Dipende perchè non tutte le response di fastAPI sono json
+    //   //
+    //   // // 1- $contents viene jsonencodato 2 volte
+    //   //
+    //   // return new JsonResponse([ 'data' => $contents, 'method' => 'GET', 'status'=> $status]);
+    //
+    // }
 
 
     public function ServiceUrl(Request $request) {
@@ -126,7 +126,7 @@ class QgisServer extends ControllerBase {
 
       $url =  $QgisUrl.'?'.http_build_query($GET_params);
 
-      \Drupal::service('messenger')->addMessage("<code>".print_r($url,TRUE)."</code>");
+      // \Drupal::service('messenger')->addMessage("<code>".print_r($url,TRUE)."</code>");
 
 
       $request = $this->httpClient->request('GET', $url);
